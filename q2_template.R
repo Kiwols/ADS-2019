50b218e4fec9d56f899f0b6bb399654be7d864dc
Kiwols <sebastian.slowik@stud-mail.uni-wuerzburg.de>
2019-05-16 19:30:03 +0200

    Aufgabe2. a/b

--- q2_template.R
+++ q2_template.R
@@ -6,34 +6,40 @@
 
 library(RCurl)
 library(RJSONIO)
+library(dplyr)
 URL = "https://www.googleapis.com/books/v1/volumes?q=george+r+r+martin&maxResults=40"
 response_parsed <- fromJSON(getURL(URL,ssl.verifyhost = 0L, ssl.verifypeer = 0L))
 
 data.class(response_parsed)
 
 
-print(response_parsed$items[[12]])
+print(getItems(response_parsed$items[[15]]))
       
-
+print(map_df(response_parsed$items,getItems))
 
 
 #a)
 # Your code
-getItems <- function(items){
- data.frame( Title = c( ifelse(is.null(items$volumeInfo$title),"-",items$volumeInfo$title))
-             Autor = c( ifelse(is.null(items$volumeInfo$authors),"-",items$volumeInfo$authors))
-             Publishing_Date = c( ifelse(is.null(items$volumeInfo$title),"-",items$volumeInfo$title))
-             Rating = c( ifelse(is.null(items$volumeInfo$title),"-",items$volumeInfo$title)))
-}
 
 
 #b)
 # Your code
 
+getItems <- function(items){
+  data.frame( Title = c( ifelse(is.null(items$volumeInfo$title),"-",substring(items$volumeInfo$title,1,20))),
+              Autor = c( ifelse(is.null(items$volumeInfo$authors),"-",items$volumeInfo$authors)),
+              Publishing_Date = c( ifelse(is.null(items$volumeInfo$publishedDate),"-",items$volumeInfo$publishedDate)),
+              Rating = c( ifelse(is.null(items$volumeInfo$maturityRating),"-",items$volumeInfo$maturityRating)))
+  
+}
+
+
 #c) 
-#getBookList = function(numberOfItems) {
+getBookList = function(numberOfItems) {
   # Your code
-#}
+
+  
+}
 
 #d)
 #getBookSalesInfo = function(response) {
